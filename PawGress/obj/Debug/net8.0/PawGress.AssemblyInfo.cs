05c8093489a5ad27affe1242ea009e5fec47d724
Macky <macahilas.johnmichael@ue.edu.ph>
2025-11-11 11:20:15 +0000

    first

--- PawGress/obj/Debug/net8.0/PawGress.AssemblyInfo.cs
+++ PawGress/obj/Debug/net8.0/PawGress.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PawGress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613a4d87a167b5bec7ea6983bb3ecae01247ef2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eeefda5a5da8d5a16d0da7308eed5f0e78a539e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PawGress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PawGress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
